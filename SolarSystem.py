1330c0331b2dccd030233d5a2f1c3046cec69d4f
huksleva <leonid005xc@gmail.com>
2025-05-23 17:43:39 +0300

    123

--- SolarSystem.py
+++ SolarSystem.py
@@ -1,56 +1,12 @@
 import turtle
 import time
-import math
-from operator import index
+from constants import *
 
 # Настройка экрана
 screen = turtle.Screen()
 screen.bgcolor("black")
 screen.setup(1920, 1080)
 screen.title("Симуляция Солнечной системы")
-
-# Все данные взяты с сайта NASA
-# Радиусы в метрах
-r_Sun = 700000000
-r_Mercurian = 2440000
-r_Venus = 6051800
-r_Earth = 6371000
-r_Mars = 3390000
-r_Jupiter = 69911000
-r_Saturn = 58232000
-r_Uranus = 25362000
-r_Neptune = 24622000
-
-# Дистанция от Солнца до планеты в метрах
-dist_Mercurian = 579*(10^5)
-dist_Venus = 1082*(10^5)
-dist_Earth = 1496*(10^5)
-dist_Mars = 2279*(10^5)
-dist_Jupiter = 7786*(10^5)
-dist_Saturn = 14335*(10^5)
-dist_Uranus = 28725*(10^5)
-dist_Neptune = 44951*(10^5)
-
-# Массы Солнца и планет в кг
-m_Sun = 1.989*(10^30)
-m_Mercurian = 3.3*(10^23)
-m_Venus = 4.9*(10^24)
-m_Earth = 5.97*(10^24)
-m_Mars = 6.42*(10^23)
-m_Jupiter = 1.89*(10^27)
-m_Saturn = 5.68*(10^26)
-m_Uranus = 8.68*(10^25)
-m_Neptune = 1.02*(10^26)
-
-# Начальные скорости планет в м/c
-spd_Mercurian = 47870
-spd_Venus = 35020
-spd_Earth = 29780
-spd_Mars = 24130
-spd_Jupiter = 13070
-spd_Saturn = 9690
-spd_Uranus = 6810
-spd_Neptune = 5430
 
 
 # Масштаб всей симуляции. 0 < global_scale < 1 - уменьшить масштаб, 1 < global_scale - увеличить масштаб
