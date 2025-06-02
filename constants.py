aaabfdffb05c9765c6c2c9ae48132ae4da4d41e4
huksleva <leonid005xc@gmail.com>
2025-06-02 18:47:17 +0300

    123

--- constants.py
+++ constants.py
@@ -1,10 +1,3 @@
-
-# Гравитационная постоянная. Данные взяты из статьи журнала Nature: https://www.nature.com/articles/s41586-018-0431-5
-# G = const
-G = 6.674334e-11
-
-
-
 # Данные взяты с сайта NASA. Все значения представлены в СИ. Все значения = const.
 # Радиусы - м.
 # Массы - кг.
@@ -17,81 +10,63 @@ G = 6.674334e-11
 planets_data = {
     "Sun": {
         "radius": 7e8,
-        "mass": 1.989e30,
         "distanceFromSun": 0,
-        "speed": 0,
         "color": "yellow", # Жёлтый
         "orbitalPeriod": 0,
         "eccentricity": 0
     },
     "Mercurian": {
         "radius": 244e4,
-        "mass": 3.3e23,
         "distanceFromSun": 579e8,
-        "speed": 47870,
         "color": "#A9A9A9", # Темно-серый, скалистый
         "orbitalPeriod": 7600688,
         "eccentricity": 0.206
     },
     "Venus": {
         "radius": 6051800,
-        "mass": 4.9e24,
         "distanceFromSun": 1082e8,
-        "speed": 35020,
         "color": "#FFFFCC", # Белесовато-желтый
         "orbitalPeriod": 19410240,
         "eccentricity": 0.007
     },
     "Earth": {
         "radius": 6371e3,
-        "mass": 5.97e24,
         "distanceFromSun": 1496e8,
-        "speed": 29780,
         "color": "#3D85C6", #
         "orbitalPeriod": 31558151,
         "eccentricity": 0.017
     },
     "Mars": {
         "radius": 339e4,
-        "mass": 6.42e23,
         "distanceFromSun": 2279e8,
-        "speed": 24130,
         "color": "#CC6600", #
         "orbitalPeriod": 59353728,
         "eccentricity": 0.093
     },
     "Jupiter": {
         "radius": 69911e3,
-        "mass": 1.89e27,
         "distanceFromSun": 7786e8,
-        "speed": 13070,
         "color": "#F2D4A2", #
         "orbitalPeriod": 374434176,
         "eccentricity": 0.048
     },
     "Saturn": {
         "radius": 58232e3,
-        "mass": 5.68e26,
         "distanceFromSun": 14335e8,
-        "speed": 9690,
         "color": "#E3C891", #
         "orbitalPeriod": 930752640,
         "eccentricity": 0.056
     },
     "Uranus": {
         "radius": 25362e3,
-        "mass": 8.68e25,
         "distanceFromSun": 28725e8,
-        "speed": 6810,
         "color": "#B2FFFF", #
         "orbitalPeriod": 2659213440,
         "eccentricity": 0.047
     },
     "Neptune": {
         "radius": 24622e3,
-        "mass": 1.02e26,
         "distanceFromSun": 44951e8,
-        "speed": 5430,
         "color": "#3D66FF", #
         "orbitalPeriod": 5202620160,
         "eccentricity": 0.009
@@ -147,5 +122,4 @@ planets_data_multipliers = {
         "distanceFromSun": 1 / 2.5 * smaller_dist,
     }
 }
-
 
